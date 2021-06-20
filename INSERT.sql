71dde56402ab6a653f22bd61384640b3cb5f0997
StanislavPonomarev <stanislov@inbox.ru>
2021-06-20 22:50:51 +0300

    DZ_5 «Группировки, выборки из нескольких таблиц»

--- INSERT.sql
+++ INSERT.sql
@@ -1,237 +1,102 @@
 INSERT INTO groups
-VALUES( 1, 'Руки вверх');
-
-INSERT INTO groups
-VALUES( 2, 'Ногу свело');
-
-INSERT INTO groups
-VALUES( 3, 'Агата гристи');
-
-INSERT INTO groups
-VALUES( 4, 'Филлип Киркоров');
-
-INSERT INTO groups
-VALUES( 5, 'Тимати');
-
-INSERT INTO groups
-VALUES( 6, 'Баста');
-
-INSERT INTO groups
-VALUES( 7, 'Маргенштерн');
-
-INSERT INTO groups
-VALUES( 8, 'Моцарт');
-
+VALUES
+    ( 1, 'Руки вверх'),
+    ( 2, 'Ногу свело'),
+    ( 3, 'Агата гристи'),
+    ( 4, 'Филлип Киркоров'),
+    ( 5, 'Тимати'),
+    ( 6, 'Баста'),
+    ( 7, 'Маргенштерн'),
+    ( 8, 'Моцарт');
 
 INSERT INTO albums
-VALUES( 1, 'без тормозов', '2018-01-01 00:00:00');
-
-INSERT INTO albums
-VALUES( 2, 'Хочу Бодаться!', '2020-01-01 00:00:00');
-
-INSERT INTO albums
-VALUES( 3, 'Опиум', '1995-01-01 00:00:00');
-
-INSERT INTO albums
-VALUES( 4, 'я', '2016-01-01 00:00:00');
-
-INSERT INTO albums
-VALUES( 5, 'мой Транзит', '2020-01-01 00:00:00');
-
-INSERT INTO albums
-VALUES( 6, 'Nintento', '2011-01-01 00:00:00');
-
-INSERT INTO albums
-VALUES( 7, 'Легендарная пыль', '2020-01-01 00:00:00');
-
-INSERT INTO albums
-VALUES( 8, 'Реквием', '1970-01-01 00:00:00');
-
+VALUES
+    ( 1, 'без тормозов', '2018-01-01 00:00:00'),
+    ( 2, 'Хочу Бодаться!', '2020-01-01 00:00:00'),
+    ( 3, 'Опиум', '1995-01-01 00:00:00'),
+    ( 4, 'я', '2016-01-01 00:00:00'),
+    ( 5, 'мой Транзит', '2020-01-01 00:00:00'),
+    ( 6, 'Nintento', '2011-01-01 00:00:00'),
+    ( 7, 'Легендарная пыль', '2020-01-01 00:00:00'),
+    ( 8, 'Реквием', '1970-01-01 00:00:00');
 
 INSERT INTO track_list
-VALUES( 1, 1, 'Без любви', '00:01:00');
-
-INSERT INTO track_list
-VALUES( 2, 1, 'Мой Атаман', '00:02:00');
-
-INSERT INTO track_list
-VALUES( 3, 2, 'Самурай', '00:03:00');
-
-INSERT INTO track_list
-VALUES( 4, 2, 'Фол', '00:04:00');
-
-INSERT INTO track_list
-VALUES( 5, 3, 'ХалиГалиКришна', '00:05:00');
-
-INSERT INTO track_list
-VALUES( 6, 3, 'Тоска без конца', '00:06:00');
-
-INSERT INTO track_list
-VALUES( 7, 4, 'Второе Я', '00:07:00');
-
-INSERT INTO track_list
-VALUES( 8, 4, 'Лунный гость', '00:07:00');
-
-INSERT INTO track_list
-VALUES( 9, 5, 'Харлей', '00:03:00');
-
-INSERT INTO track_list
-VALUES( 10, 5, 'Loco', '00:04:00');
-
-INSERT INTO track_list
-VALUES( 11, 6, 'Интро', '00:06:00');
-
-INSERT INTO track_list
-VALUES( 12, 6, 'Отмели', '00:02:00');
-
-INSERT INTO track_list
-VALUES( 13, 7, 'Я пыль', '00:03:00');
-
-INSERT INTO track_list
-VALUES( 14, 7, 'Съел деда', '00:04:00');
-
-INSERT INTO track_list
-VALUES( 15, 8, 'Реквием2', '00:05:00');
-
+VALUES
+    ( 1, 1, 'Без любви', '00:01:00'),
+    ( 2, 1, 'Мой Атаман', '00:02:00'),
+    ( 3, 2, 'Самурай', '00:03:00'),
+    ( 4, 2, 'Фол', '00:04:00'),
+    ( 5, 3, 'ХалиГалиКришна', '00:05:00'),
+    ( 6, 3, 'Тоска без конца', '00:06:00'),
+    ( 7, 4, 'Второе Я', '00:07:00'),
+    ( 8, 4, 'Лунный гость', '00:07:00'),
+    ( 9, 5, 'Харлей', '00:03:00'),
+    ( 10, 5, 'Loco', '00:04:00'),
+    ( 11, 6, 'Интро', '00:06:00'),
+    ( 12, 6, 'Отмели', '00:02:00'),
+    ( 13, 7, 'Я пыль', '00:03:00'),
+    ( 14, 7, 'Съел деда', '00:04:00'),
+    ( 15, 8, 'Реквием2', '00:05:00');
 
 INSERT INTO collections
-VALUES( 1, 'Коллеция 1', '2016-01-19 00:00:00');
-
-INSERT INTO collections
-VALUES( 2, 'Коллеция 2', '2016-01-20 00:00:00');
-
-INSERT INTO collections
-VALUES( 3, 'Коллеция 3', '2016-01-21 00:00:00');
-
-INSERT INTO collections
-VALUES( 4, 'Коллеция 4', '2016-01-22 00:00:00');
-
-INSERT INTO collections
-VALUES( 5, 'Коллеция 5', '2016-01-23 00:00:00');
-
-INSERT INTO collections
-VALUES( 6, 'Коллеция 6', '2016-01-24 00:00:00');
-
-INSERT INTO collections
-VALUES( 7, 'Коллеция 7', '2018-01-25 00:00:00');
-
-INSERT INTO collections
-VALUES( 8, 'Коллеция 8', '2016-01-26 00:00:00');
-
-INSERT INTO collections
-VALUES( 9, 'Коллеция 9', '2016-01-27 00:00:00');
-
+VALUES
+    ( 1, 'Коллеция 1', '2016-01-19 00:00:00'),
+    ( 2, 'Коллеция 2', '2016-01-20 00:00:00'),
+    ( 3, 'Коллеция 3', '2016-01-21 00:00:00'),
+    ( 4, 'Коллеция 4', '2016-01-22 00:00:00'),
+    ( 5, 'Коллеция 5', '2016-01-23 00:00:00'),
+    ( 6, 'Коллеция 6', '2016-01-24 00:00:00'),
+    ( 7, 'Коллеция 7', '2018-01-25 00:00:00'),
+    ( 8, 'Коллеция 8', '2016-01-26 00:00:00'),
+    ( 9, 'Коллеция 9', '2016-01-27 00:00:00');
 
 INSERT INTO genre_music
-VALUES( 1, 'Поп');
-
-INSERT INTO genre_music
-VALUES( 2, 'РЭП');
-
-INSERT INTO genre_music
-VALUES( 3, 'Рок');
-
-INSERT INTO genre_music
-VALUES( 4, 'Шляпа');
-
-INSERT INTO genre_music
-VALUES( 5, 'Классика');
-
+VALUES
+    ( 1, 'Поп'),
+    ( 2, 'РЭП'),
+    ( 3, 'Рок'),
+    ( 4, 'Шляпа'),
+    ( 5, 'Классика');
 
 INSERT INTO groups_albums
-VALUES( 1, 1);
-
-INSERT INTO groups_albums
-VALUES( 2, 2);
-
-INSERT INTO groups_albums
-VALUES( 3, 3);
-
-INSERT INTO groups_albums
-VALUES( 4, 4);
-
-INSERT INTO groups_albums
-VALUES( 5, 5);
-
-INSERT INTO groups_albums
-VALUES( 6, 6);
-
-INSERT INTO groups_albums
-VALUES( 7, 7);
-
-INSERT INTO groups_albums
-VALUES( 8, 8);
-
+VALUES
+    ( 1, 1),
+    ( 2, 2),
+    ( 3, 3),
+    ( 4, 4),
+    ( 5, 5),
+    ( 6, 6),
+    ( 7, 7),
+    ( 8, 8);
 
 INSERT INTO groups_genre
-VALUES( 1, 1);
-
-INSERT INTO groups_genre
-VALUES( 2, 3);
-
-INSERT INTO groups_genre
-VALUES( 3, 3);
-
-INSERT INTO groups_genre
-VALUES( 4, 1);
-
-INSERT INTO groups_genre
-VALUES( 5, 2);
-
-INSERT INTO groups_genre
-VALUES( 6, 2);
-
-INSERT INTO groups_genre
-VALUES( 7, 4);
-
-INSERT INTO groups_genre
-VALUES( 8, 5);
-
+VALUES
+    ( 1, 1),
+    ( 2, 3),
+    ( 3, 3),
+    ( 4, 1),
+    ( 5, 2),
+    ( 6, 2),
+    ( 7, 4),
+    ( 8, 5);
 
 INSERT INTO track_collections
-VALUES( 1, 1);
-
-INSERT INTO track_collections
-VALUES( 2, 2);
-
-INSERT INTO track_collections
-VALUES( 3, 3);
-
-INSERT INTO track_collections
-VALUES( 3, 4);
-
-INSERT INTO track_collections
-VALUES( 4, 5);
-
-INSERT INTO track_collections
-VALUES( 4, 6);
-
-INSERT INTO track_collections
-VALUES( 4, 7);
-
-INSERT INTO track_collections
-VALUES( 5, 8);
-
-INSERT INTO track_collections
-VALUES( 5, 9);
-
-INSERT INTO track_collections
-VALUES( 6, 10);
-
-INSERT INTO track_collections
-VALUES( 6, 11);
-
-INSERT INTO track_collections
-VALUES( 7, 12);
-
-INSERT INTO track_collections
-VALUES( 8, 13);
-
-INSERT INTO track_collections
-VALUES( 8, 14);
-
-INSERT INTO track_collections
-VALUES( 9, 14);
+VALUES
+    ( 1, 1),
+    ( 2, 2),
+    ( 3, 3),
+    ( 3, 4),
+    ( 4, 5),
+    ( 4, 6),
+    ( 4, 7),
+    ( 5, 8),
+    ( 5, 9),
+    ( 6, 10),
+    ( 6, 11),
+    ( 7, 12),
+    ( 8, 13),
+    ( 8, 14),
+    ( 9, 14);
 
 
 
